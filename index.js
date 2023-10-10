a27d6200018c9225ce4c335801af31614d1e2ada
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-10-10 17:45:30 +1100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+function writeCards(names, event) {
+    let messArray = []
+    for( let i = 0; i< names.length ; i++){
+        messArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messArray
+}
+
+const countDown = () => {
+    let i = 0
+    while (i < 11){
+        console.log(i++)
+    }
+}
